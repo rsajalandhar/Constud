f04a565d177481050eb11b9b8076699fb4e3b524
rsajalandhar <sandhusachin04@gmail.com>
2017-03-27 21:44:38 +0530

    Initial commit

---
+++ app/src/main/java/com/chips/rsa/constud/Landing.java
@@ -0,0 +1,20 @@
+package com.chips.rsa.constud;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+    }
+}
